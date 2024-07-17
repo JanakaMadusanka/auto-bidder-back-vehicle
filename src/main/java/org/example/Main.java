8720838d3bd796103728bac5c1a9edee958c9d3d
janaka <jnk.madusanka86@gmail.com>
2024-07-17 09:04:50 +0530

    dependencies installed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("autoBidder-be-vehicle");
+        SpringApplication.run(Main.class);
     }
 }
